13bb5e9121d45fb7eb767f1b137addcfa5932093
Ypp6657 <yash-dev1@demo.lab>
2024-12-26 16:14:30 +0530

    Initialized 3

--- dhanno.py
+++ dhanno.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 
 def dhanno():
-    return "Welcome to KUBERNETES!!!!"
+    return "I am bored.....tomorrow I will have Old Monk and Jaggermite with some Jenkins!!!!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=7000, debug=True)
